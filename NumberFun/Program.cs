9c1686a653009df8a96ba998a362a879cb0a124b
David Steedman <dave@voxace.com>
2017-10-18 09:58:39 +1100

    We completed the number fun program

---
+++ NumberFun/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberFun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number 1 :");
+            float num1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter number 2 :");
+            float num2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine(num1 + " plus " + num2 + " = " + (num1 + num2));
+            Console.WriteLine($"{num1} minus {num2} = {(num1 - num2)}");
+
+            Console.ReadKey();
+        }
+    }
+}

f3f26c7a5352636b7b3db2240bfe3c93330094da
David Steedman <dave@voxace.com>
2017-10-20 09:47:15 +1100

    Completed age program

---
+++ AgeProgram/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your age:");
+            int age = int.Parse(Console.ReadLine());
+
+            if (age < 13)
+            {
+                Console.WriteLine("You are a Child!");
+            }
+            else if(age < 20)
+            {
+                Console.WriteLine("You are a Teenager!");
+            }
+            else
+            {
+                Console.WriteLine("You are an Adult");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PickADoor/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickADoor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a door");
+            string choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                Console.WriteLine("Behind door 1 is Bo Browning");
+            }
+            else if (choice == "2")
+            {
+                Console.WriteLine("Behind door 2 is Bo again");
+            }
+            else if (choice == "3")
+            {
+                Console.WriteLine("Behind door 3 is Mr. Bo Browning");
+            }
+            else
+            {
+                Console.WriteLine("Bo's not happy");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}

2ab07b0c2111f2eb9230f9649d8a67fe3c0bd69d
David Steedman <dave@voxace.com>
2017-10-24 14:57:51 +1100

    Made loops program

---
+++ Loops/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("start");
+            Console.ReadLine();
+
+            int number = 1;
+
+            while(number <= 10)
+            {
+                Console.WriteLine(number);
+                number++;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
